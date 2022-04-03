99cfba5ff13c125adb394095ef6fcced73da9a73
Jorge Castro <JCastro@uline.com>
2022-04-03 14:13:11 -0500

    Updating logic.js file

--- Mapping_GeoJSON_Linestrings/js/logic.js
+++ Mapping_GeoJSON_Linestrings/js/logic.js
@@ -5,8 +5,8 @@
 //let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
@@ -20,15 +20,15 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  Light: light,
   Dark: dark
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [30, 30],
+  center: [44.0, -80.0],
   zoom: 2,
-  layers: [streets]
+  layers: [light]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -38,7 +38,8 @@ L.control.layers(baseMaps).addTo(map);
 //streets.addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/JCastro1986/Mapping_Earthquakes/main/majorAirports.json";
+//let airportData = "https://raw.githubusercontent.com/JCastro1986/Mapping_Earthquakes/main/majorAirports.json";
+let torontaData = "https://raw.githubusercontent.com/JCastro1986/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
 
 // // Add GeoJSON data.
 // let sanFranAirport =
@@ -63,15 +64,28 @@ let airportData = "https://raw.githubusercontent.com/JCastro1986/Mapping_Earthqu
 // // Grabbing our GeoJSON data.
 // L.geoJSON(sanFranAirport).addTo(map);
 
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
+
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontaData).then(function(data) {
     console.log(data);
     // onEachFeature: function (feature, layer) {
     //     console.log(layer);
     //     layer.bindPopup("<h2>" + "Airport Code:" + airportData.feature.faa + "</h2> <hr> <h3> " + "Airport name:" + airportData.feature.name + "</h3>");
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
-    
+  L.geoJSON(data, {
+    style: myStyle, 
+    //color: "#ffffa1",
+    //weight: 2,  
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h3> + Airline: " + feature.properties.airline + "</h3> <hr> <h3> Destination: " + feature.properties.dst + "</h3>");
+    }
+  })
+.addTo(map);
 });
 
 // Grabbing our GeoJSON data.
